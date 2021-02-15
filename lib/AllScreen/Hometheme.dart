c3559b6409a0692861a141b12a2bce0930583f0d
witsanu01 <76748174+witsanu01@users.noreply.github.com>
2021-02-15 19:11:42 +0700

    add

--- lib/AllScreen/Hometheme.dart
+++ lib/AllScreen/Hometheme.dart
@@ -1,47 +1,124 @@
-import 'package:Health/AllScreen/mainScreen.dart';
-import 'package:Health/AllScreen/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-class Hometheme extends StatelessWidget {
-  static const String idScreen = "Hometheme";
+
+class ThemeBuilder extends StatefulWidget {
+  final Widget Function(BuildContext context, Brightness brightness) builder;
+  final Brightness defaultBrightness;
+
+  ThemeBuilder({this.builder, this.defaultBrightness});
+
+  @override
+  _ThemeBuilderState createState() => _ThemeBuilderState();
+
+  static _ThemeBuilderState of(BuildContext context)
+  {
+    return context.ancestorStateOfType(const TypeMatcher<_ThemeBuilderState>());
+  }
+}
+
+class _ThemeBuilderState extends State<ThemeBuilder> {
+  Brightness _brightness;
+
+  @override
+  void initState() {
+    super.initState();
+    _brightness = widget.defaultBrightness;
+
+    if(mounted)
+      setState(() {
+
+      });
+  }
+
+  void changeTheme() {
+    setState(() {
+      _brightness =
+      _brightness == Brightness.dark ? Brightness.light : Brightness.dark;
+    });
+  }
+
+  Brightness getCurrentTheme()
+  {
+    return _brightness;
+  }
+
   @override
   Widget build(BuildContext context) {
-    ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
-    return new Scaffold(
+    return widget.builder(context, _brightness);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    ThemeBuilder.of(context).changeTheme();
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        elevation: 0.0,
-        title: Text(
-          'ThemeHome',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.fast_forward_outlined,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              Navigator.pushNamedAndRemoveUntil(
-                  context,mainScreen.idScreen, (route) => false);
-            },
-          )
-        ],
+        title: Text(widget.title),
       ),
-      body: Container(
+      body: Center(
         child: Column(
-          children: [
-            FlatButton(
-                child: Text('Dark Theme'),
-                onPressed: ()=>_themeChanger.setTheme(ThemeData.dark())),
-            FlatButton(
-                child: Text('Light Theme'),
-                onPressed: ()=>_themeChanger.setTheme(ThemeData.light())),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DetailPage()));
+              },
+              child: Text('Go to Detail'),
+            ),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
 }
+
+class DetailPage extends StatefulWidget {
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to Detail Page!'),
+      ),
+    );
+  }
+}
+
+
